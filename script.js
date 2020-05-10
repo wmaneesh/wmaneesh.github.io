97c222631daa6dc583c0697862c08130d9e8c371
wmaneesh <36091416+wmaneesh@users.noreply.github.com+wmaneesh@outlook.com>
2020-05-10 04:41:15 -0400

    back to old hamburger nav bar

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
-//interactivel nav with toggle
+//burger nav bar animation
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
-const navClose = () => {
-  const navLinks = document.querySelectorAll(".nav-links");
+  burger.addEventListener("click", () => {
+    //toggle nav
+    nav.classList.toggle("nav-active");
 
-  navLinks.addEventListener("click", () => {
-    nav.classList.toggle("nav-deactive");
+    //animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`; //index/7 gives you a delay
+      }
+    });
+    //burger animation
+    burger.classList.toggle("toggle");
   });
 };
+
+
+navSlide();
+
+// let number = document.getElementById("phone-text");
+
+// // This handler will be executed only once when the cursor
+// // moves over the text
+// number.addEventListener("mouseover", function (event) {
+//   // change the text when the mouse is over it
+//   //   number.style.opacity = 0;
+//   number.innerHTML = "416-841-8915";
+
+//   // reset the text after a short delay
+//   //   setTimeout(function () {
+//   //     // number.style.opacity = 1;
+//   //     number.innerHTML = "416-841-8915";
+//   //   }, 1000);
+// });
+
+// number.addEventListener("onmouseout", function (event) {
+//   //   number.style.opacity = 1;
+
+//   setTimeout(function () {
+//     number.innerHTML = "&nbsp &nbsp &nbsp &nbsp Phone &nbsp &nbsp &nbsp";
+//     // number.style.opacity = 1;
+//   }, 2500);
+// });
 
 //scroll reveal for the about text and contact links
 const sr = ScrollReveal({ viewOffset: { top: 65 } });
@@ -31,38 +74,12 @@ sr.reveal(".contact-links a", {
   useDelay: "once",
 });
 
-//toggle for nav button and nav links
-$(document).ready(function () {
-  $(".toggle-nav").click(function () {
-    $(".toggle-nav").toggleClass("active-nav");
-    $(".overlay-nav").toggleClass("active-nav");
-    $(".menu-nav").toggleClass("active-nav");
-  });
-  $(".home").click(function () {
-    $(".toggle-nav").toggleClass("active-nav");
-    $(".overlay-nav").toggleClass("active-nav");
-    $(".menu-nav").toggleClass("active-nav");
-  });
-  $(".about").click(function () {
-    $(".toggle-nav").toggleClass("active-nav");
-    $(".overlay-nav").toggleClass("active-nav");
-    $(".menu-nav").toggleClass("active-nav");
-  });
-  $(".projects").click(function () {
-    $(".toggle-nav").toggleClass("active-nav");
-    $(".overlay-nav").toggleClass("active-nav");
-    $(".menu-nav").toggleClass("active-nav");
-  });
-  $(".contacts").click(function () {
-    $(".toggle-nav").toggleClass("active-nav");
-    $(".overlay-nav").toggleClass("active-nav");
-    $(".menu-nav").toggleClass("active-nav");
-  });
-});
-
 var anchor = "";
 document.documentElement.dataset.scroll = 0;
 var about = document.getElementById("about");
 
-navSlide();
-navClose();
+// $(function () {
+//   $(".nav-links a").click(function () {
+//     anchor = $(this).attr("href");
+//   });
+// });
